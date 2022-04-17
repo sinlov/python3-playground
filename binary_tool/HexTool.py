2089b04d1280f16c7bf84b6a7181c8059f628c13
sinlov <sinlovgmppt@gmail.com>
2022-04-17 23:24:30 +0800

    feat: add HexTool and test case

---
+++ binary_tool/HexTool.py
@@ -0,0 +1,26 @@
+class HexTool:
+
+    @staticmethod
+    def hex_str_to_bytes(hex_str):
+        # type: (str) -> bytes
+        return bytes.fromhex(hex_str)
+
+    @staticmethod
+    def bytes_to_hex_str(bs):
+        # type: (bytes) -> str
+        return ''.join(['%02X' % b for b in bs])
+
+    @staticmethod
+    def bytes_to_hex_str_arr(bs):
+        # type: (bytes) -> str
+        return ''.join(['%02X ' % b for b in bs])
+
+    @staticmethod
+    def str_to_bytes(target):
+        # type: (str) -> bytes
+        return bytes(target, encoding='utf8')
+
+    @staticmethod
+    def bytes_to_str(bs):
+        # type: (bytes) -> str
+        return bytes.decode(bs, encoding='utf8')
